4be8e273ed269e2fe458b09067d204f3bb3c2d8e
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2023-09-02 12:01:49 +0300

    Add files via upload

---
+++ seminars/task_1.py
@@ -0,0 +1,22 @@
+# Создайте класс МояСтрока где будут доступны все возможности str 
+# и дополнительно хранится имя автора строки и время создания (time.time)
+
+import time
+
+
+class MyString(str):
+    """Расширенный класс строки."""
+
+    def __new__(cls, value: str, name: str):
+        instance = super().__new__(cls, value)
+        instance.name = name
+        instance.created_at = time.time()
+        return instance
+
+
+if __name__ == '__main__':
+    mystr = MyString('САМА строка', 'доп. параметр')
+    print(mystr.name)
+    print(mystr.created_at)
+    print(mystr)
+    print(mystr.upper())
